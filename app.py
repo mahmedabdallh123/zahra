d29e81bb21bbb3411bed43ec402d32c22a4fe364
mahmed abdallh <medotatch124@gmail.com>
2026-08-24 00:58:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2755,7 +2755,7 @@ with tabs[idx]:
         }}
         .scrolling-content {{
             display: inline-block;
-            animation: scroll-text 1000s linear infinite;  /* زيادة المدة من 20s إلى 45s (أبطأ) */
+            animation: scroll-text 1080s linear infinite;  /* زيادة المدة من 20s إلى 45s (أبطأ) */
             font-size: 40px;  /* زيادة حجم الخط من 18px إلى 26px */
             font-weight: bold;
             color: #1a1a2e;
